822a954a3a9becbbdec6e297bd154784c6613785
Talia Ringer <tringer@cs.washington.edu>
2019-12-04 16:05:35 -0800

    wip

--- plugin/coq/PreprocessModule.v
+++ plugin/coq/PreprocessModule.v
@@ -2,15 +2,24 @@ Require Import Fixtranslation.Fixtoelim.
 Require List.
 
 (*
- * Test whole module preprocessing to convert fixpoints
- * to induction principles. By Nate Yazdani, from DEVOID.
+ * Test whole module preprocessing to convert fixpoints to induction principles.
+ * By Nate Yazdani with later additions by Talia Ringer.
  *)
 
 (*
- * NOTE: Any serious bug is likely to cause a typing error, and comparing the
- * exact output against some reference would give negligible further assurance
- * at the cost of unwieldiness. It would be very difficult to translate terms
- * only partially while preserving well-typedness.
+ * This now recursively goes in and preprocesses dependent modules, ignoring
+ * only the terms we tell it to ignore. Here, we document why we choose to ignore
+ * certain terms we don't support yet.
  *)
-Preprocess Module List as List'. (* TODO some subterms are broken *)
+Preprocess Module List as List' { opaque (* ignore these: *)
+  (* dependent elimination only: *)
+  RelationClasses.StrictOrder_Transitive
+  RelationClasses.StrictOrder_Irreflexive
+  RelationClasses.Equivalence_Symmetric
+  RelationClasses.Equivalence_Transitive
+  RelationClasses.PER_Symmetric
+  RelationClasses.PER_Transitive
+  RelationClasses.Equivalence_Reflexive
+  
+}.
 Print List'.
