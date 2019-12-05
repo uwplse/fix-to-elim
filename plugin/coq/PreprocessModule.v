321d63d699a6ee0bd20315a6d006c702c9683510
Talia Ringer <tringer@cs.washington.edu>
2019-12-04 16:15:07 -0800

    Figure out all terms to ignore to get tests working

--- plugin/coq/PreprocessModule.v
+++ plugin/coq/PreprocessModule.v
@@ -2,11 +2,6 @@ Require Import Fixtranslation.Fixtoelim.
 Require List.
 
 (*
- * Test whole module preprocessing to convert fixpoints to induction principles.
- * By Nate Yazdani with later additions by Talia Ringer.
- *)
-
-(*
  * This now recursively goes in and preprocesses dependent modules, ignoring
  * only the terms we tell it to ignore. Here, we document why we choose to ignore
  * certain terms we don't support yet.
@@ -20,6 +15,8 @@ Preprocess Module List as List' { opaque (* ignore these: *)
   RelationClasses.PER_Symmetric
   RelationClasses.PER_Transitive
   RelationClasses.Equivalence_Reflexive
-  
+  (* proofs about these match over the above opaque terms, and would fail: *)
+  Nat.add
+  Nat.sub
 }.
 Print List'.
