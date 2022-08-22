7ee901725994ce6498ff51416bce7a107ee5f422
Max Fan <git@max.fan>
2022-08-22 16:35:38 -0500

    Fix desugar_constr bug (need to return term, not type)

--- plugin/src/automation/desugar.ml
+++ plugin/src/automation/desugar.ml
@@ -344,8 +344,8 @@ let desugar_constr env sigma trm =
   let sigma, trm' = aux env (sigma, trm) in
   let eterm = EConstr.of_constr trm' in
   try
-    let (sigma, typ) = Typing.type_of ~refresh:true env sigma eterm (* might be able to set refresh to false *) in
-    sigma, EConstr.to_constr sigma typ
+    let (sigma, _) = Typing.type_of ~refresh:true env sigma eterm (* might be able to set refresh to false *) in
+    sigma, trm'
   with PretypeError (env, sigma, err) ->
     user_err
       "desugar"
